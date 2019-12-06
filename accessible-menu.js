ffdedfe5046640ff5050cda27099359db7bdc27c
thomasmester <thomas@blue4you.be>
2019-12-06 08:43:22 +0100

    update doc add conditional to execute the script

--- accessible-menu.js
+++ accessible-menu.js
@@ -6,173 +6,175 @@
  */
 
 (function($){
-  $.fn.accessibleMenu = function (options) {
-    // Default variable
-    var el = "#" + this[0].id; // Element
-  
-    // Defaults settings
-    var defaults = {
-      animation: "none"
-    };
-  
-    var settings = $.extend({}, defaults, options);
-  
-    // Hide all sub menus if the controller is expanded false
-    $(el + ' button[aria-expanded="false"]')
-      .next("ul")
-      .each(function () {
-        $(this).hide();
-      });
-  
-    // Handle click
-    $("body").on("click", el + " button", function () {
-      accessibleMenuToggler($(this));
-    });
-  
-    // Handle keyboard
-    this.keydown(function (e) {
-      e = e || window.event;
-      var focused = $(":focus");
-  
-      if (e.keyCode == "27") {
-        // ESC
-        if (focused.is('button[aria-expanded="true"]')) {
-          accessibleMenuToggler(focused);
-        } else {
-          var button = focused
-            .parent()
-            .parent()
-            .parent()
-            .children("button");
-          button.focus();
-          accessibleMenuToggler(button);
-        }
-      } else if (e.keyCode == "37" || e.keyCode == "38") {
-        // ARROW LEFT & UP
-        e.preventDefault();
-        focused
-          .parent()
-          .prev("li")
-          .children("a, button")
-          .focus();
-      } else if (e.keyCode == "39" || e.keyCode == "40") {
-        // ARROW RIGHT & DOWN
-        e.preventDefault();
-        if(focused.is('button[aria-expanded="true"]')){
-          focused
-            .next("ul")
-            .children('li:first-child')
-            .children("a, button")
-            .focus();
-        } else {
-          focused
-            .parent()
-            .next("li")
-            .children("a, button")
-            .focus();
-        }
-      } else if (e.keyCode == "36") {
-        // HOME
-        e.preventDefault();
-        focused
-          .parent()
-          .parent()
-          .children('li:first-child')
-          .children('a, button')
-          .focus();
-      } else if (e.keyCode == "35") {
-        // END
-        e.preventDefault();
-        focused
-          .parent()
-          .parent()
-          .children("li:last-child")
-          .children('a, button')
-          .focus();
-      } else {
-        // Normal behavior
-      }
-    });
-  
-    // Toggler
-    function accessibleMenuToggler(item) {
-      var depthString = " > ul > li ";
-  
-      if (item == false) {
-        $(el + ' button[aria-expanded="true"]').each(function () {
-          $(this).attr("aria-expanded", false);
-          animate($(this), 'close');
+  $.fn.accessibleMenu = function (options) {    
+    if (this.length) {
+      // Default variable
+      var el = "#" + this[0].id; // Element
+
+      // Defaults settings
+      var defaults = {
+        animation: "none"
+      };
+    
+      var settings = $.extend({}, defaults, options);
+    
+      // Hide all sub menus if the controller is expanded false
+      $(el + ' button[aria-expanded="false"]')
+        .next("ul")
+        .each(function () {
+          $(this).hide();
         });
-      } else {
-        $(el + depthString.repeat(getCurrentDepth(item)) + ' button[aria-expanded="true"]')
-          .not(item)
-          .each(function () {
+    
+      // Handle click
+      $("body").on("click", el + " button", function () {
+        accessibleMenuToggler($(this));
+      });
+    
+      // Handle keyboard
+      this.keydown(function (e) {
+        e = e || window.event;
+        var focused = $(":focus");
+    
+        if (e.keyCode == "27") {
+          // ESC
+          if (focused.is('button[aria-expanded="true"]')) {
+            accessibleMenuToggler(focused);
+          } else {
+            var button = focused
+              .parent()
+              .parent()
+              .parent()
+              .children("button");
+            button.focus();
+            accessibleMenuToggler(button);
+          }
+        } else if (e.keyCode == "37" || e.keyCode == "38") {
+          // ARROW LEFT & UP
+          e.preventDefault();
+          focused
+            .parent()
+            .prev("li")
+            .children("a, button")
+            .focus();
+        } else if (e.keyCode == "39" || e.keyCode == "40") {
+          // ARROW RIGHT & DOWN
+          e.preventDefault();
+          if(focused.is('button[aria-expanded="true"]')){
+            focused
+              .next("ul")
+              .children('li:first-child')
+              .children("a, button")
+              .focus();
+          } else {
+            focused
+              .parent()
+              .next("li")
+              .children("a, button")
+              .focus();
+          }
+        } else if (e.keyCode == "36") {
+          // HOME
+          e.preventDefault();
+          focused
+            .parent()
+            .parent()
+            .children('li:first-child')
+            .children('a, button')
+            .focus();
+        } else if (e.keyCode == "35") {
+          // END
+          e.preventDefault();
+          focused
+            .parent()
+            .parent()
+            .children("li:last-child")
+            .children('a, button')
+            .focus();
+        } else {
+          // Normal behavior
+        }
+      });
+    
+      // Toggler
+      function accessibleMenuToggler(item) {
+        var depthString = " > ul > li ";
+    
+        if (item == false) {
+          $(el + ' button[aria-expanded="true"]').each(function () {
             $(this).attr("aria-expanded", false);
             animate($(this), 'close');
           });
-  
-        var ariaExpanded = item.attr("aria-expanded") == "true" ? false : true;
-        item.attr("aria-expanded", ariaExpanded);
-        animate(item, 'toggle');
-      }
-    }
-  
-    // To know which level you are in the menu
-    function getCurrentDepth(element) {
-      var depth = 0;
-  
-      for (i = 0; i < element.parents().length; i++) {
-        if (element.parents()[i].tagName == "NAV") {
-          /*
-            We devide by 2 because otherwise it count </li> like a level of depth
-            Example:
-            ul > li > ul > li => 4 parents but a depth of 2
-            So depth = parents / 2
-          */
-          depth = i / 2;
-          break;
+        } else {
+          $(el + depthString.repeat(getCurrentDepth(item)) + ' button[aria-expanded="true"]')
+            .not(item)
+            .each(function () {
+              $(this).attr("aria-expanded", false);
+              animate($(this), 'close');
+            });
+    
+          var ariaExpanded = item.attr("aria-expanded") == "true" ? false : true;
+          item.attr("aria-expanded", ariaExpanded);
+          animate(item, 'toggle');
         }
-      }  
-      return depth;
-    }
-
-    // Annimate function
-    function animate(element, action) {
-      switch (settings.animation) {
-        case "fade":
-          if(action == 'close') {
-            element.next("ul").fadeOut();
-          } else if(action == 'open'){
-            element.next("ul").fadeIn();
-          } else {
-            element.next("ul").fadeToggle();
+      }
+    
+      // To know which level you are in the menu
+      function getCurrentDepth(element) {
+        var depth = 0;
+    
+        for (i = 0; i < element.parents().length; i++) {
+          if (element.parents()[i].tagName == "NAV") {
+            /*
+              We devide by 2 because otherwise it count </li> like a level of depth
+              Example:
+              ul > li > ul > li => 4 parents but a depth of 2
+              So depth = parents / 2
+            */
+            depth = i / 2;
+            break;
           }
-          break;
-        case "slide":
-            if(action == 'close') {
-              element.next("ul").slideUp();
-            } else if(action == 'open'){
-              element.next("ul").slideDown();
-            } else {
-              element.next("ul").slideToggle();
-            }
-          break;
-        default:
-            if(action == 'close') {
-              element.next("ul").hide();
-            } else if(action == 'open'){
-              element.next("ul").show();
-            } else {
-              element.next("ul").toggle();
-            }
-        }
-    }
-  
-    $(el + ", " + el + " *").focusout(function (e) {
-      if (!$(e.relatedTarget).is(el + ", " + el + " *")) {
-        accessibleMenuToggler(false);
+        }  
+        return depth;
       }
-    });
+
+      // Annimate function
+      function animate(element, action) {
+        switch (settings.animation) {
+          case "fade":
+            if(action == 'close') {
+              element.next("ul").fadeOut();
+            } else if(action == 'open'){
+              element.next("ul").fadeIn();
+            } else {
+              element.next("ul").fadeToggle();
+            }
+            break;
+          case "slide":
+              if(action == 'close') {
+                element.next("ul").slideUp();
+              } else if(action == 'open'){
+                element.next("ul").slideDown();
+              } else {
+                element.next("ul").slideToggle();
+              }
+            break;
+          default:
+              if(action == 'close') {
+                element.next("ul").hide();
+              } else if(action == 'open'){
+                element.next("ul").show();
+              } else {
+                element.next("ul").toggle();
+              }
+          }
+      }
+    
+      $(el + ", " + el + " *").focusout(function (e) {
+        if (!$(e.relatedTarget).is(el + ", " + el + " *")) {
+          accessibleMenuToggler(false);
+        }
+      });
+    }
 
     // polyfill
     if (!String.prototype.repeat) {
